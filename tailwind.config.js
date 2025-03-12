a3277937eb01ca6fc15528e6c98dbf24e183dd87
PierreML-06 <pierre.mouilleseaux-lhuillier@edu.univ-fcomte.fr>
2025-03-12 11:50:57 +0100

    rajoute de page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,webp}"],
   theme: {
     fontFamily: {
       sans: ["Plus Jakarta Sans", "sans-serif"],
